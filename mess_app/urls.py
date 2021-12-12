25a89064254444317c0d4d27a80181cc9826b835
rahul <rahul@surecash.net>
2021-12-13 02:36:40 +0600

    Updated mess->mess_info & mess_duration->mess_event on APIs

--- mess_app/urls.py
+++ mess_app/urls.py
@@ -3,9 +3,9 @@ from rest_framework.routers import DefaultRouter
 from mess_app.views import MessEventViewSet, MessViewSet, MessNUserViewSet
 
 router = DefaultRouter()
-router.register(r'mess',MessViewSet)
+router.register(r'mess_info',MessViewSet)
 router.register(r'mess_n_user', MessNUserViewSet)
-router.register(r'mess_duration', MessEventViewSet)
+router.register(r'mess_event', MessEventViewSet)
 
 urlpatterns = []
 urlpatterns += router.urls
