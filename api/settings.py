29691e256c9c64b1bf8271372e060ed3cf45541f
rahul <rahul@surecash.net>
2022-05-22 12:17:03 +0600

    Added Gunicon related conf file

--- api/settings.py
+++ api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[])
 
 
 # Application definition
