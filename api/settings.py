8ac9cce8a159ff3044acf9f38d98b42f18f3075c
rahul <rahul@surecash.net>
2022-08-11 13:37:40 +0600

    Updated gunicorn & static file serve related stuff on env.example, settings & readme

--- api/settings.py
+++ api/settings.py
@@ -168,7 +168,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR("static")
+STATIC_ROOT = env.str("STATIC_FILES_ROOT", default="static")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR("assets"),
